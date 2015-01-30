c639af018d78d4c13a8d241f2b0c807647960b6b
johnsilvester <johnsilvester15@gmail.com>
2015-01-30 15:31:28 -0700

    adding overall layout

---
+++ Accomplish/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Accomplish
+//
+//  Created by John Silvester on 1/30/15.
+//  Copyright (c) 2015 PaxtonTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+@property(strong,nonatomic) NSArray *settings;
+@end
